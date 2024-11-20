cc4e5ad92c3faafe772fd9c43b3ca9e457604323
Ayomidekayo <kayodeayomide677@gmail.com>
2024-11-20 18:02:39 +0100

    Add project files.

---
+++ StudentEnrolment.Data/BaseEntity.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentEnrolment.Data
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string MordifiedBy { get; set; }
+        public DateTime MordifiedDate { get; set; }
+    }
+}
+
+
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LasttName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string IsNumber { get; set; }
+    public string Picture { get; set; }
+}
+
+public class Course
+{
+    public int Title { get; set; }
+    public string CreatedBy { get; set; }
+}
+public class Enrollment
+{
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public virtual Student Student { get; set; }
+    public virtual Student Student { get; set; }
+}
+
---
+++ StudentEnrolment.Data/Course.cs
@@ -0,0 +1,8 @@
+﻿using StudentEnrolment.Data;
+
+public class Course:BaseEntity
+{
+    public int Title { get; set; }
+    public string CreatedBy { get; set; }
+}
+
